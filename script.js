20007843fb57f45d1ae0a94fc0b108047f66f914
Audreyjrq-11 <ajrquibin@gmail.com>
2025-03-16 16:24:46 +0800

    hero

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const nav = document.querySelector('.nav');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heroContent = document.querySelector('.hero-content');
+    heroContent.classList.add('show');
+});
